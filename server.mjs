7d7c4448090b7d5cf0a1c233f6ac3d8ed2b698b2
du693 <100497517+du693@users.noreply.github.com>
2023-11-25 14:42:35 -0500

    security additions

--- server.mjs
+++ server.mjs
@@ -1,10 +1,16 @@
 import express from "express";
 import next from "next";
+import https from "https";
+import fs from "fs";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443; // Change the port to 443 for HTTPS
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const privateKey = fs.readFileSync("/path/to/private-key.pem", "utf8"); // Replace with the actual path to your private key
+const certificate = fs.readFileSync("/path/to/certificate.pem", "utf8"); // Replace with the actual path to your certificate
+const credentials = { key: privateKey, cert: certificate };
 
 app.prepare().then(() => {
 	const server = express();
@@ -16,8 +22,11 @@ app.prepare().then(() => {
 		return handle(req, res);
 	});
 
-	server.listen(port, (err) => {
+	// Create an HTTPS server
+	const httpsServer = https.createServer(credentials, server);
+
+	httpsServer.listen(port, "0.0.0.0", (err) => {
 		if (err) throw err;
-		console.log(`> Ready on http://localhost:${port}`);
+		console.log(`> Ready on https://0.0.0.0:${port}`);
 	});
 });
