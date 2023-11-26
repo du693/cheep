8129525e5b30090705b123392ec574d3af76dc55
du693 <100497517+du693@users.noreply.github.com>
2023-11-26 12:46:50 -0500

    security config 10/26

--- server.mjs
+++ server.mjs
@@ -1,22 +1,10 @@
 import express from "express";
 import next from "next";
-import https from "https";
-import fs from "fs";
 
-const port = process.env.PORT || 443; // Change the port to 443 for HTTPS
+const port = process.env.PORT || 3000; // Change the port to 3000 for HTTP
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
-const privateKey = fs.readFileSync(
-	"/etc/letsencrypt/live/cheepbirds.com/privkey.pem",
-	"utf8"
-);
-const certificate = fs.readFileSync(
-	"/etc/letsencrypt/live/cheepbirds.com/fullchain.pem",
-	"utf8"
-); // Replace with the actual path to your certificate
-const credentials = { key: privateKey, cert: certificate };
 
 app.prepare().then(() => {
 	const server = express();
@@ -28,11 +16,8 @@ app.prepare().then(() => {
 		return handle(req, res);
 	});
 
-	// Create an HTTPS server
-	const httpsServer = https.createServer(credentials, server);
-
-	httpsServer.listen(port, "cheepbirds.com", (err) => {
+	server.listen(port, (err) => {
 		if (err) throw err;
-		console.log(`> Ready on https://cheepbirds.com:${port}`);
+		console.log(`> Ready on http://localhost:${port}`);
 	});
 });
